43df4de11ea98749c20e3df105b7c575b71aab72
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-19 17:10:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-function letterCombinations(input_digit) {
+function letterCombinations(digit) {
   //Complete the function
+	 const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+    '0': '0',
+    '1': '1'
+  };
+
+  // Handle empty input
+  if (digits.length === 0) {
+    return [];
+  }
+
+  const combinations = [''];
+
+  for (const digit of digits) {
+    const letters = digitToLetters[digit];
+    const newCombinations = [];
+
+    for (const combination of combinations) {
+      for (const letter of letters) {
+        newCombinations.push(combination + letter);
+      }
+    }
+
+    combinations.length = 0;
+    combinations.push(...newCombinations);
+  }
+
+  return combinations;
 }
 
 module.exports = letterCombinations;
